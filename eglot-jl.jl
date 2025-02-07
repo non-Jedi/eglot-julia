0208bbf8f7c10941eb5e86e99c940129db95f375
François Févotte <francois.fevotte@elmy.fr>
2025-02-07 14:14:52 +0100

    First draft for an "environment management system"

--- eglot-jl.jl
+++ eglot-jl.jl
@@ -6,22 +6,58 @@
 # being available on LOAD_PATH
 import Pkg
 
-# Resolving the environment is necessary for cases where the shipped
-# Manifest.toml is not compatible with the Julia version.
-for _ in 1:2
-    try
-        Pkg.resolve(io=stderr)
-        @info "Environment successfully resolved"
-        break
-    catch err
-        # Downgrading from 1.6 to 1.5 sometimes causes temporary errors
-        @warn "Error while resolving the environment; retrying..." err
-    end
+# By default, look for environments stored alongside eglot-jl's sources
+const ENV_PATH = [@__DIR__]
+
+# ... but users can provide a writable location to store their environments
+if "EGLOT_JL_ENVDIR" ∈ keys(ENV)
+    mkpath(ENV["EGLOT_JL_ENVDIR"])
+    pushfirst!(ENV_PATH, ENV["EGLOT_JL_ENVDIR"])
 end
 
-# In julia 1.4 this operation takes under a second. This can be
-# crushingly slow in older versions of julia though.
-Pkg.instantiate()
+# Look for all already instantiated environments with
+# compatible Julia versions
+envs = Iterators.flatten(readdir(path, join=true) for path in ENV_PATH)
+envs = map(envs) do dir
+    ispath(joinpath(dir, "Project.toml")) || return nothing
+
+    m = match(r"env-(.+)", dir)
+    isnothing(m) && return nothing
+
+    v = VersionNumber(m[1])
+    v > VERSION && return nothing
+
+    return (v, dir)
+end
+filter!(!isnothing, envs) |> sort!
+foreach(envs) do (version, dir)
+    @debug "Found compatible environment" version dir
+end
+
+# Our best candidate is the one with the latest compatible version
+(base_ver, base_dir) = last(envs)
+
+if base_ver == VERSION
+    # A suitable environment has been found: simply instantiate it
+    Pkg.activate(base_dir)
+    Pkg.instantiate()
+else
+    # No suitable environment can be found: create one
+    env_dir = joinpath(first(ENV_PATH), "env-$(VERSION)")
+
+    @info "Creating new version-specific environment" env_dir base_dir
+    mkpath(env_dir)
+    cp(joinpath(base_dir, "Project.toml"),
+       joinpath(env_dir,  "Project.toml"))
+
+    if (ispath(joinpath(base_dir, "Manifest.toml")))
+        cp(joinpath(base_dir, "Manifest.toml"),
+           joinpath(env_dir,  "Manifest.toml"))
+    end
+
+    Pkg.activate(env_dir)
+    Pkg.resolve()
+end
 
 # Get the source path. In order of increasing priority:
 # - default value:  pwd()
